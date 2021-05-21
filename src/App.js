f0258da60760ded2f64a87f7737304bc7c7e167f
cuongvu3009 <cuongvu3009@gmail.com>
2021-05-21 23:38:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
+}
+
+export default App;
